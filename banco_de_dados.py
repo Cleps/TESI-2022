c782c94e8e5dc479d7df0ca4b170850c31bc6861
Cleps <104847424+Cleps@users.noreply.github.com>
2022-09-12 11:04:02 -0500

    Update banco_de_dados.py

--- banco_de_dados.py
+++ banco_de_dados.py
@@ -1,7 +1,6 @@
+
 import sqlite3
 from sqlite3 import Error
-
-
 
 #con faz a conexão
 def conectar_banco():
@@ -14,11 +13,21 @@ def conectar_banco():
         print(erro)
 
 
-sql_insert = 'INSERT INTO cliente VALUES("3","teste","12312312322")'
-
 def inserir(sql):
     con = conectar_banco()
     cursor = con.cursor()
     cursor.execute(sql)
     print('Registro inserido com sucesso')    
     con.commit()
+    con.close()
+
+
+sql_insert = 'INSERT INTO clientes VALUES("3","teste","12312312322")'
+
+dele = 'DELETE FROM clientes WHERE id=1'
+
+inserir()
+
+
+#a = input('')
+#inserir(a)
